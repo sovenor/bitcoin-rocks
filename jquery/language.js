12cd34dcf2466bb3be84c08e9cbfc49c9bc04812
sovenor <hi@bitcoin.rocks>
2025-07-03 21:01:09 -0400

    force CSS & language.js refresh on all pages to show new content

--- jquery/language.js
+++ jquery/language.js
@@ -2,7 +2,7 @@ $(function() {
   'use strict';
 
   // Version for cache busting - increment this when updating translations
-  var TRANSLATION_VERSION = '1.0.3';
+  var TRANSLATION_VERSION = '1.0.4';
 
   // Initialize i18n
   var i18n = $.i18n();
