a2512a2f55f168316fd0c019b4277287894db7ed
sovenor <hi@bitcoin.rocks>
2025-07-16 15:51:56 -0400

    cache busting to show new content

--- jquery/language.js
+++ jquery/language.js
@@ -2,7 +2,7 @@ $(function() {
   'use strict';
 
   // Version for cache busting - increment this when updating translations
-  var TRANSLATION_VERSION = '1.0.8';
+  var TRANSLATION_VERSION = '1.0.9';
 
   // Initialize i18n
   var i18n = $.i18n();
