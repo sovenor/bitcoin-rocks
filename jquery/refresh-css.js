b79f376b0d0493834b1efe987448c2f2bc671066
sovenor <hi@bitcoin.rocks>
2026-01-26 12:29:28 -0500

    remove deprecated cache buster

--- jquery/refresh-css.js
+++
@@ -1,9 +0,0 @@
-/* currently disabled
-var pathSegments = window.location.pathname.split('/');
-var relativePath = '';
-for (var i = 1; i < pathSegments.length - 1; i++) {
-    relativePath += '../';
-}
-var timestamp = new Date().getTime();
-document.getElementById('style-link').href = relativePath + 'css/style.css?v=' + timestamp;
-*/
