036c099d9545174dd853da58e38eabb41a99ea7a
sovenor <hi@bitcoin.rocks>
2026-03-14 19:24:50 -0400

    cached translation version 1.1.5 > 1.1.6

--- jquery/language.js
+++ jquery/language.js
@@ -2,7 +2,7 @@ $(function() {
   'use strict';
 
   // Version for cache busting - increment this when updating translations
-  var TRANSLATION_VERSION = '1.1.5';
+  var TRANSLATION_VERSION = '1.1.6';
 
   // Initialize i18n
   var i18n = $.i18n();
